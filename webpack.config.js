87b4089cfd5f090cdc5ddb783a5daa57c8e9ad42
Maaz <syed_maaz_hasan@yahoo.com>
2020-04-19 18:27:30 +0530

    added all loaders

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,40 @@ var path = require("path");
 
 var baseConfig = {
   entry: {
-    main: "./script1.js",
+    main: "./index.html",
   },
 
   output: {
     filename: "main2.js",
     path: path.resolve("./build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)?$/,
+        use: [{ loader: "babel-loader" }],
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: "raw-loader" }],
+      },
+      {
+        test: /\.(gif|jpe?g|png|ico)$/,
+        use: [{ loader: "url-loader" }],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [{ loader: "file-loader" }],
+      },
+      {
+        test: /\.svg$/,
+        use: [{ loader: "url-loader" }],
+      },
+      {
+        test: /\.(p)?css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+    ],
   },
 };
 
